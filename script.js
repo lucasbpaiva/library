6c6bbc0fdfaa5c810c3f1f3f64115eb148873e8b
Lucas Paiva <lpaiva234@gmail.com>
2025-06-04 10:45:20 -0300

    add button to toggle read status

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-let myLibrary = [
-    {title: "Fahrenheit 451", author: "Ray Bradbury", pages: 199, read: true, id: "82723be9-a71c-48ca-a068-730c49534642"},
-    {title: "Pachinko", author: "Min Jin Lee", pages: 522, read: true, id: "ff1eb1b5-66de-47b1-a605-6e57fdfbba49"},
-    {title: "1984", author: "George Orwell", pages: 362, read: false, id: "8d86f374-d4af-40f5-9b1d-7abec52459f4"},
-    {title: "To Kill a Mockingbird", author: "Harper Lee", pages: 349, read: true, id: "b80fe826-0e7d-4f63-b0b2-8811e0906dbd"},
-    {title: "Kafka on the Shore", author: "Haruki Murakami", pages: 571, read: true, id: "403c9a9f-591d-43ae-91d0-3ee060480855"},
-    {title: "An Artist of the Floating World", author: "Kazuo Ishiguro", pages: 206, read: false, id: "c9f3b294-ec5f-4ce2-8956-bfc717c54f58"}];
+let myLibrary = [];
 
 function Book(title, author, pages, read, id) {
     if (!new.target) {
@@ -17,12 +11,24 @@ function Book(title, author, pages, read, id) {
     this.id = id;
 }
 
+Book.prototype.toggleReadStatus = function() {
+    this.read = !this.read;
+};
+
 function addBookToLibrary(title, author, pages, read) {
     let id = crypto.randomUUID();
     let book = new Book(title, author, pages, read, id);
     myLibrary.push(book);
     return book
 }
+
+//add some books manually
+addBookToLibrary("Fahrenheit 451", "Ray Bradbury", 199, true);
+addBookToLibrary("Pachinko", "Min Jin Lee", 522, true);
+addBookToLibrary("1984", "George Orwell", 362, false);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 349, true);
+addBookToLibrary("Kafka on the Shore", "Haruki Murakami", 571, true);
+addBookToLibrary("An Artist of the Floating World", "Kazuo Ishiguro", 206, false);
 
 function createBookCard(book) {
     let container = document.querySelector(".container")
@@ -49,18 +55,34 @@ function createBookCard(book) {
     read.textContent = book.read ? "Already Read" : "Not read yet";
     infoContainer.appendChild(read);
 
+    card.dataset.id = book.id;
     card.appendChild(infoContainer);
 
-    card.dataset.id = book.id;
+    let buttonsContainer = document.createElement("div");
+    buttonsContainer.classList.add("buttons-container");
+
+    let toggleReadBtn = document.createElement("button");
+    toggleReadBtn.textContent = book.read ? "Mark as not finished" : "Mark as read";
+    toggleReadBtn.classList.add("card-button");
+    buttonsContainer.appendChild(toggleReadBtn);
+
+    toggleReadBtn.addEventListener("click", () => {
+        //toggle object property
+        book.toggleReadStatus();
+        //toggle button textContent
+        toggleReadBtn.textContent = book.read ? "Mark as not finished" : "Mark as read";
+        //toggle card textContent
+        read.textContent = book.read ? "Already Read" : "Not read yet";
+    });
 
     let removeBtn = document.createElement("button");
     removeBtn.textContent = "Remove";
-    removeBtn.classList.add("remove-button");
-    card.appendChild(removeBtn);
+    removeBtn.classList.add("card-button");
+    buttonsContainer.appendChild(removeBtn);
 
     removeBtn.addEventListener("click", (event) => {
-        let cardToRemove = event.target.parentElement;
-        let bookId = event.target.parentElement.dataset.id;
+        let cardToRemove = event.target.parentElement.parentElement;
+        let bookId = cardToRemove.dataset.id;
 
         //remove card
         container.removeChild(cardToRemove);
@@ -68,6 +90,7 @@ function createBookCard(book) {
         myLibrary = myLibrary.filter(book => book.id != bookId);
     });
 
+    card.appendChild(buttonsContainer);
     container.appendChild(card);
 }
 
